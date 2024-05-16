fe5de264f8dfe36d94b718d8477965b1c6cd5059
samsama <hsdr0258@gmail.com>
2024-05-16 20:17:56 +0700

    buoi-5

---
+++ src/components/Product.jsx
@@ -0,0 +1,15 @@
+import product1 from "./assets/images/quan.webp";
+
+<div class="col">
+  <div class="card">
+    <img src={product1} class="card-img-top" alt="Sản phẩm 1" />
+    <div class="card-body">
+      <h5 class="card-title">Sản phẩm 1</h5>
+      <p class="card-text">Mô tả sản phẩm 1</p>
+      <p class="card-text">Giá: 100.000 VNĐ</p>
+      <a href="#" class="btn btn-primary">
+        Xem thêm
+      </a>
+    </div>
+  </div>
+</div>;
